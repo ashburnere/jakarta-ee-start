0003673042eded369602af3ab9edabe1c30781a7
Erik Aschenbrenner <ashburnere@gmail.com>
2019-01-11 15:02:59 +0100

    Add dynamic query to get all Todos to QueryService

--- src/main/java/com/ashburnere/todo/control/QueryService.java
+++ src/main/java/com/ashburnere/todo/control/QueryService.java
@@ -1,10 +1,23 @@
 package com.ashburnere.todo.control;
 
+import java.util.List;
+
 import javax.enterprise.context.ApplicationScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
+
+import com.ashburnere.todo.entity.Todo;
 
 @Transactional
 @ApplicationScoped
 public class QueryService {
 
+	@PersistenceContext
+	EntityManager em;
+
+	public List<Todo> getAllTodos() {
+		// query all Todo objects using JPQL
+		return em.createQuery("select todo from Todo todo order by todo.dueDate", Todo.class).getResultList();
+	}
 }
