f205e310dcabb02165b04edc580e3aa161f1fe00
Erik Aschenbrenner <ashburnere@gmail.com>
2019-01-11 14:39:03 +0100

    Activate CDI for all beans and add 2 services

---
+++ src/main/java/com/ashburnere/todo/control/QueryService.java
@@ -0,0 +1,10 @@
+package com.ashburnere.todo.control;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
+
+@Transactional
+@ApplicationScoped
+public class QueryService {
+
+}
